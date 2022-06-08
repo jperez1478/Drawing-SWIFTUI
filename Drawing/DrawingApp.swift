f051a135075af7050bdbe8e63fdccb763dab2f08
jperez1478 <jessicperez3@gmail.com>
2022-06-08 17:45:21 -0500

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Jessica Perez on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
